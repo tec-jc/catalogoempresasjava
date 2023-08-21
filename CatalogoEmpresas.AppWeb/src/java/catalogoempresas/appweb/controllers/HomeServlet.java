261b66bf78d01c72e8eca671f2318b4cf3b0a89d
JC Tula <jtula_esfe@outlook.es>
2023-08-21 10:44:21 -0600

    Codificación de controladores

--- CatalogoEmpresas.AppWeb/src/java/catalogoempresas/appweb/controllers/HomeServlet.java
+++ CatalogoEmpresas.AppWeb/src/java/catalogoempresas/appweb/controllers/HomeServlet.java
@@ -1,7 +1,6 @@
 package catalogoempresas.appweb.controllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,5 +10,15 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet(name = "HomeServlet", urlPatterns = {"/Home"})
 public class HomeServlet extends HttpServlet {
 
+    private void doGetRequestIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGetRequestIndex(request, response);
+    }
     
 }
